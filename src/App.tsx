2bfa5e005e45621713f55dea0ba164e6f8fd031c
James Brightman <jamesmbrightman@gmail.com>
2023-01-02 23:15:31 +0000

    init hook getting data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react"
+import {firestore} from "./firebase-setup/firebase"
+import {useCollectionData} from 'react-firebase-hooks/firestore'
+
+export interface EntryObject {
+  rating: number
+}
 
 export const App = () => {
 
+  // const collection = firestore.collection('entries');
+  // const query = collection.limit(25) as EntryObject[];
+
+  const [data, loading, error] = useCollectionData<EntryObject>(
+    firestore.collection('entries') as any
+  );
+  console.log(data, loading, error)
+
   return (
-    <div>abc</div>
+    <div>
+      {data?.map((ele: EntryObject) => {return <div>{ele.rating}</div>})}
+    </div>
   )
 }

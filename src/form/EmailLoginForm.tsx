210af63310a5786a619e6b72e6f728b39762430d
James <jamesmbrightman@gmail.com>
2023-01-12 17:17:36 +0000

    Improve login

--- src/form/EmailLoginForm.tsx
+++ src/form/EmailLoginForm.tsx
@@ -3,7 +3,9 @@ import { FormProvider, useForm } from "react-hook-form";
 import { IconButton } from "../components/IconButton";
 import { CalmFormProvider } from "./components/CalmFormProvider";
 import { TextInput } from "./components/TextInput";
-import {MdLogin} from "react-icons/md";
+import { MdLogin } from "react-icons/md";
+import { HiOutlineMail } from "react-icons/hi";
+import { FiLock } from "react-icons/fi";
 
 export interface EmailLoginFormProps {
   onSubmit: (email: string, password: string) => void;
@@ -20,7 +22,7 @@ export const EmailLoginForm: FC<EmailLoginFormProps> = ({ onSubmit }) => {
   });
 
   const submitEmailLogin = (data: EmailLoginFields) => {
-    console.log(data)
+    console.log(data);
     onSubmit(data.email, data.password);
   };
 
@@ -34,20 +36,27 @@ export const EmailLoginForm: FC<EmailLoginFormProps> = ({ onSubmit }) => {
         <TextInput
           formName="email"
           label="Email"
-          placeholder={"Enter email address..."}
+          InputIcon={<HiOutlineMail className="w-full h-full" />}
           required
         />
 
         <TextInput
           formName="password"
           label="Password"
-          placeholder={"Enter your password..."}
+          InputIcon={<FiLock className="w-full h-full" />}
           required
+          type="password"
         />
 
-         <span className="w-full pb-0.5 bg-gray-300 mb-6 mt-2"></span>
+        <span className="w-full pb-0.5 bg-gray-300 mb-6 mt-2"></span>
 
-        <IconButton buttonText="Sign In" handleClick={() => {}} icon={<MdLogin className="w-8 h-8"/>} type="submit" className="text-lg font-bold bg-blue-600 text-white p-4 w-72"/>
+        <IconButton
+          buttonText="Sign In"
+          handleClick={() => {}}
+          icon={<MdLogin className="w-8 h-8" />}
+          type="submit"
+          className="text-lg font-bold bg-blue-600 text-white p-4 w-72"
+        />
       </CalmFormProvider>
     </div>
   );
--- src/form/components/TextInput.tsx
+++ src/form/components/TextInput.tsx
@@ -1,16 +1,17 @@
-import { FC, InputHTMLAttributes, useEffect } from "react";
+import { FC, InputHTMLAttributes } from "react";
 import { useFormContext } from "react-hook-form";
 
 export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   formName: string;
-  placeholder?: string;
+  InputIcon?: JSX.Element;
 }
 
 export const TextInput: FC<TextInputProps> = ({
   label,
   formName,
   placeholder,
+  InputIcon,
   ...props
 }) => {
   const { register, setValue } = useFormContext();
@@ -18,16 +19,21 @@ export const TextInput: FC<TextInputProps> = ({
   return (
     <div className="flex flex-col gap-1">
       <p className="px-2 text-lg font-bold">{label}</p>
-      <input
-        className="p-2 text-md border border-2 rounded-full"
-        placeholder={placeholder}
-        {...register(formName)}
-        {...props}
-        onChange={(newValue) => {
-          setValue(formName, newValue.target.value, { shouldDirty: true });
-          // onChange && onChange(newValue);
-        }}
-      />
+      <div className="flex flex-row p-2 w-full text-md border border-2 rounded-full items-center justify-center">
+        {InputIcon && (
+          <span className="w-6 h-6 text-gray-400">{InputIcon}</span>
+        )}
+        <input
+          className="w-full px-2"
+          placeholder={placeholder}
+          {...register(formName)}
+          onChange={(newValue) => {
+            setValue(formName, newValue.target.value, { shouldDirty: true });
+            // onChange && onChange(newValue);
+          }}
+          {...props}
+        />
+      </div>
     </div>
   );
 };

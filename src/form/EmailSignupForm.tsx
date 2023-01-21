6c9b9bf0212bd3418ee86e9f62a6fd1c8c96e124
James <jamesmbrightman@gmail.com>
2023-01-21 13:40:34 +0000

    Init signup

--- src/form/EmailSignupForm.tsx
+++ src/form/EmailSignupForm.tsx
@@ -1,29 +1,72 @@
 import { useForm } from "react-hook-form";
 import { FC } from "react";
+import { PasswordInput } from "./components/PasswordInput";
+import { CalmFormProvider } from "./components/CalmFormProvider";
+import { TextInput } from "./components/TextInput";
+import { HiOutlineMail } from "react-icons/hi";
+import { IconButton } from "../components/IconButton";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/firebaseInit";
+import { SignupError } from "../utils/errors/SignupError";
+import { ErrorChit } from "../components/ErrorChit";
+import { BsFillPersonFill } from "react-icons/bs";
 
 export interface EmailSignupFormProps {
   onSubmit: () => void;
 }
 
+interface EmailSignupFields {
+  email: string;
+  password: string;
+}
+
 export const EmailSignupForm: FC<EmailSignupFormProps> = ({ onSubmit }) => {
-  const { register, handleSubmit } = useForm();
+  const methods = useForm<EmailSignupFields>({
+    shouldFocusError: true,
+  });
+
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    createUserLoading,
+    createUserError,
+  ] = useCreateUserWithEmailAndPassword(auth);
+
+  const submitEmailSignup = async (data: EmailSignupFields) => {
+    createUserWithEmailAndPassword(data.email, data.password);
+  };
 
   return (
     <div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-2 p-4"
+      <CalmFormProvider
+        methods={methods}
+        onSubmit={(e) => {
+          console.log(e);
+        }}
+        className="flex flex-col gap-2 p-4 w-full items-center justify-center"
       >
-        <label>
-          First name
-          <input {...register("fistName")} className="border-2" />
-        </label>
-        <label>
-          Last name
-          <input {...register("lastName")} className="border-2" />
-        </label>
-        <input type={"submit"} />
-      </form>
+        <TextInput
+          formName="email"
+          label="Email"
+          InputIcon={<HiOutlineMail className="w-6 h-6 m-[8px]" />}
+          required
+          validationMessage="Invalid email address"
+        />
+        <PasswordInput formName="password" label="Password" />
+        {createUserError && (
+          <ErrorChit>{SignupError(createUserError)}</ErrorChit>
+        )}
+        <span className="w-full pb-0.5 bg-gray-300 mb-6 mt-2"></span>
+        <IconButton
+          buttonText="Sign Up"
+          handleClick={() => {}}
+          loading={createUserLoading}
+          icon={<BsFillPersonFill className="w-8 h-8" />}
+          type="submit"
+          variant="contained"
+          className="font-semibold bg-blue-600 text-white p-2 w-72"
+        />
+      </CalmFormProvider>
     </div>
   );
 };
---
+++ src/utils/errors/SignupError.ts
@@ -0,0 +1,8 @@
+import { AuthError } from "firebase/auth";
+
+export const SignupError = (err: AuthError) => {
+  switch (err.code) {
+    default:
+      return "Please try again";
+  }
+};

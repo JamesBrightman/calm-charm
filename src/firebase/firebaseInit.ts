f0f0ddd631baad60d1ab3f6e1fcda2be8f57ff0b
James <jamesmbrightman@gmail.com>
2023-02-15 21:47:58 +0000

    Update firebaseInit.ts

--- src/firebase/firebaseInit.ts
+++ src/firebase/firebaseInit.ts
@@ -13,6 +13,8 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+console.log(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
